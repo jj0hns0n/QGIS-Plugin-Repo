831c2515a7f97156f5285f56c6f5d0dff78bf6b2
elpaso <apasotti@gmail.com>
2013-09-15 18:11:55 +0200

    1.5 migration
    Conflicts:
    	plugins/templates/plugins/plugin_base.html
    	plugins/templates/plugins/plugin_detail.html
    	qgis-app/settings.py
    	qgis-app/templates/admin/flatpages/flatpage/change_form.html
    	qgis-app/templates/admin_copies/change_form.html
    	qgis-app/templates/base.html
    	qgis-app/templates/cab/snippet_detail.html
    	qgis-app/templates/flatpages/homepage.html
    	qgis-app/templates/taggit_templatetags/tagcloud_include.html
    	qgis-app/users/templates/user_base.html
    	qgis-app/users/urls.py

---
+++ qgis-app/users/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls.defaults import *
+
+from django.views.generic.base import TemplateView
+
+urlpatterns = patterns('users.views',
+
+    url("^$", "usersMap", name="show_map"),
+    url("^view_users.html$", "usersMap", name="show_map"),
+    url("^create_user_form.html$", "createUser", name="create_user"),
+    url("^edit/(?P<theId>[0-9a-z\-]+)/$", "updateUser", name="update_user"),
+    url("^update_user.html$", "emailEditAddress", name="email_update_user_link"),
+)
+
+urlpatterns += patterns('django.views.generic.simple',
+    (r'^edit/email_confirm.html$',TemplateView.as_view(template_name='email_confirm.html')),
+)
